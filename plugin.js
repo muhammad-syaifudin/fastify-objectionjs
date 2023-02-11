87a636486ce5b881edc07480939e8ef7a5556b52
Muhammad Syaifudin <muhammad.syaifudin@outlook.com>
2023-02-12 03:38:17 +0700

    rename oracle to oracledb

--- plugin.js
+++ plugin.js
@@ -1,20 +1,28 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
-const Knex = require('knex')
-const { Model, knexSnakeCaseMappers } = require('objection')
+const fp = require("fastify-plugin");
+const Knex = require("knex");
+const { Model, knexSnakeCaseMappers } = require("objection");
 
 const defaultKnexConfig = {
-  client: 'better-sqlite3',
+  client: "better-sqlite3",
   useNullAsDefault: true,
   connection: {
-    filename: './default.sqlite'
-  }
-}
+    filename: "./default.sqlite",
+  },
+};
 
-const supportedClients = ['pg', 'sqlite3', 'better-sqlite3', 'mysql', 'mysql2', 'oracle', 'mssql']
+const supportedClients = [
+  "pg",
+  "sqlite3",
+  "better-sqlite3",
+  "mysql",
+  "mysql2",
+  "oracledb",
+  "mssql",
+];
 
-function fastifyObjectionjs (fastify, options, next) {
+function fastifyObjectionjs(fastify, options, next) {
   const knexConfig = Object.assign(
     {},
     defaultKnexConfig,
@@ -22,61 +30,71 @@ function fastifyObjectionjs (fastify, options, next) {
     knexSnakeCaseMappers({
       upperCase: options.upperCase || false,
       underscoreBeforeDigits: options.underscoreBeforeDigits || false,
-      underscoreBetweenUppercaseLetters: options.underscoreBetweenUppercaseLetters || false
+      underscoreBetweenUppercaseLetters:
+        options.underscoreBetweenUppercaseLetters || false,
     })
-  )
+  );
 
   if (supportedClients.indexOf(knexConfig.client) === -1) {
-    next(new Error(`unsupported client, 'fastify-objectionjs' only support ${supportedClients.join(', ')}.`))
-    return
+    next(
+      new Error(
+        `unsupported client, 'fastify-objectionjs' only support ${supportedClients.join(
+          ", "
+        )}.`
+      )
+    );
+    return;
   }
 
-  const knexConnection = Knex(knexConfig)
+  const knexConnection = Knex(knexConfig);
 
   const objection = {
-    knex: knexConnection
-  }
+    knex: knexConnection,
+  };
 
-  Model.knex(knexConnection)
+  Model.knex(knexConnection);
 
   if (options.models) {
     if (!Array.isArray(options.models) || options.models.length < 1) {
-      next(new Error('You need to provide a valid array of `objection.js` models.'))
-      return
+      next(
+        new Error("You need to provide a valid array of `objection.js` models.")
+      );
+      return;
     }
 
-    objection.models = {}
+    objection.models = {};
 
     for (let i = 0; i < options.models.length; i += 1) {
-      const model = options.models[i]
+      const model = options.models[i];
 
       if (model.idColumn && model.tableName && model.QueryBuilder) {
-        objection.models[model.name.replace(/^\w/, c => c.toLowerCase())] = model
+        objection.models[model.name.replace(/^\w/, (c) => c.toLowerCase())] =
+          model;
       }
     }
 
     if (Object.keys(objection.models).length < 1) {
-      next(new Error('The supplied models are invalid.'))
-      return
+      next(new Error("The supplied models are invalid."));
+      return;
     }
   }
 
   if (!fastify.objection) {
-    fastify.decorate('objection', objection)
+    fastify.decorate("objection", objection);
   } else {
-    next(new Error('fastify-objectionjs has already registered.'))
-    return
+    next(new Error("fastify-objectionjs has already registered."));
+    return;
   }
 
-  fastify.addHook('onClose', (fastify, done) => {
-    knexConnection.destroy()
-    done()
-  })
+  fastify.addHook("onClose", (fastify, done) => {
+    knexConnection.destroy();
+    done();
+  });
 
-  next()
+  next();
 }
 
 module.exports = fp(fastifyObjectionjs, {
-  fastify: '>=4.0.0',
-  name: 'fastify-objectionjs'
-})
+  fastify: ">=4.0.0",
+  name: "fastify-objectionjs",
+});
